a103f847c05b9817b3770c0de452839eb75ce605
pcaserta <patrick.caserta@live.longwood.edu>
2020-01-15 16:32:27 -0500

    first commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+serverId: number = 30;
+serverStatus: string = 'offline';
+ 
+
+  
+
+}
